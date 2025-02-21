f392f18910e481f80ef9b6cbc666d0baae4e4765
Alizhan Nurmagambetov <163662658+ghodinazh@users.noreply.github.com>
2025-02-21 13:46:25 +0500

    Swaggersssss
    i made some swaggers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run(); //what is this?
+if (app.Environment.IsDevelopment())
+{
+ app.UseSwagger();
+ app.UseSwaggerUI();
+}
+app.MapGet("/hello", () => new { message = "Hello, World!" });
+app.Run();
